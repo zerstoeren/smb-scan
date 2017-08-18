275ebc9e33887ffc1a7f0bfc2717cf7a90089848
root <root@docker.CISCO>
2017-08-18 02:03:03 -0400

    added captureing SMB versioning for Wannacry testing. added SMB ping type response.

--- smbscanner.py
+++ smbscanner.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 
 import smbc
+import sys
 import argparse
-import logging
 import time
 import netaddr
 import threading
-from socket import *
+from smb.SMBConnection import SMBConnection
+from socket import * # noqa
 
-def smbscan(server, results_file):
-    smb_obj = []
+
+def smbscan(server, smbversion, results_file):
+    print "attempting to find open shares on " + server + '\n'
+    #  smb_obj is needed for when we add new stuff.
+    #  currently is a place holder.
+    #  smb_obj = []
     ctx = smbc.Context()
-    ts = time.strftime("%Y-%m-%d %H:%M")
-    print "attempting to scan " + server + '\n'    
 # attempt to pull shares
     try:
+        ts = time.strftime("%Y-%m-%d %H:%M")
         entry = ctx.opendir('smb://' + server).getdents()
 #        for entry in entries:
         if entry is not None:
-            connector = socket(AF_INET, SOCK_STREAM)
+            connector = socket(AF_INET, SOCK_STREAM)  # noqa
             connector.settimeout(1)
             try:
                 connector.connect(('%s' % server, 445))
@@ -28,62 +32,110 @@ def smbscan(server, results_file):
                 if results_file is not None:
                     with print_lock:
                         with open(results_file, 'a+') as outfile:
-                            smb_data = 'host: ' + '%s' % server + '\n' + 'is_smb: true\nopen_share:' + '%s' % entry + '\n' + 'banner: ' + '%s' % smbbg + 'is_dupulsar: true\nbg_port: 445\ntimestamp: ' + '%s' % ts + '\n\n'
+                            smb_data = ('host: ' + '%s' % server + '\n' +
+                                        'is_smb: ' + smbversion + 
+                                        '\nopen_share:' +
+                                        '%s' % entry + '\n' + 'banner: ' +
+                                        '%s' % smbbg +
+                                        'is_dupulsar: true\nbg_port: 445\ntimestamp: ' +  # noqa
+                                        '%s' % ts + '\n\n')
                             outfile.write(smb_data)
                 else:
                     with print_lock:
-                        print ("[+] " + '%s' % server + ": " + '%s' % entry + ", Banner Grab: " + '%s' % smbbg + ' Possible DPulsar Target = True')
+                        print ("[+] " + '%s' % server + ": " + '%s' % entry +
+                               ", Banner Grab: " + '%s' % smbbg +
+                               ' Possible DPulsar Target: True' + 
+                               ', is_smb: ' + smbversion + '\n') 
             except:
                 if results_file is not None:
                     with print_lock:
                         with open(results_file, 'a+') as outfile:
-                            smb_data = 'host: ' + '%s' % server + '\n' + 'is_smb: true\nopen_share:' + '%s' % entry + '\n' + 'banner: closed\nis_dpulsar: false\nbg_port: 445\ntimestamp: ' + '%s' % ts + '\n\n'
+                            smb_data = ('host: ' + '%s' % server + '\n' +
+                                       'is_smb: ' + smbversion + '\nopen_share:' +
+                                       '%s' % entry + '\n' +
+                                       'banner: closed\nis_dpulsar: false\nbg_port: 445\ntimestamp: '  # noqa
+                                       + '%s' % ts + '\n\n')
                             outfile.write(smb_data)
                 else:
                     with print_lock:
-                        print ("[+] " + '%s' % server + ": " + '%s' % entry + ", Port 445: closed, Possible DPulsar Target = False")
+                        print ("[+] " + '%s' % server + ": " + '%s' % entry +
+                               ", Port 445: closed, Possible DPulsar Target = False, " +  # noqa
+                               'is_smb: ' + smbversion + '\n')  # noqa
         else:
             pass
     except:
-          pass
+        pass
+
+
+def smb_verify(server):
+    print "Validating SMB and Version for " + server + '\n'
+    try:
+        smbcon = SMBConnection('', '', server, '', use_ntlm_v2 = False)
+        assert smbcon.connect(server, 139)
+        is_smbv2 = smbcon.isUsingSMB2
+        smb_verify = smbcon.echo('EHLO', timeout=1)
+        smbcon.close()
+        if smb_verify == 'EHLO' and is_smbv2 == True:
+            smbversion = 'smbv2'
+            print "[+] " + server + ": Device is " + smbversion + '\n'
+            smbscan(server, smbversion, smbargs.results_file)
+        elif smb_verify == 'EHLO' and is_smbv2 == False:
+             smbversion = 'smbv1: potential Wannacry target'
+             print '[+] ' + server + ': Device is ' + smbversion + '\n'
+             smbscan(server, smbversion, results_file)
+        else:
+            print "[-] " + server + ": No SMB response received.\n"
+            pass
+
+    except Exception, errorcode:
+        if errorcode[1] == "Connection refused":
+            print "[-] " + server + ": Not a valid SMB device."
+            pass
+        else:
+            pass
+
+    finally:
+        pass
+
 
 def thread_check(server, results_file):
     global semaphore
 
     try:
-        smbscan(server, results_file)
+#        smb_scan(server, results_file)
+        smb_verify(server) 
     except Exception as e:
         with print_lock:
-           logger.error('Scan error at ' + server, exc_info=True)
+            logger.error('Scan error at ' + server + e, exc_info=True)  # noqa
     finally:
         semaphore.release()
 
-if __name__ == "__main__":    
+
+if __name__ == "__main__":
     smbparser = argparse.ArgumentParser(description="SMB Scanner")
-    smbparser.add_argument("-netrange", type=str, required=False, help="CIDR Block")
-    smbparser.add_argument("-ip", type=str, required=False, help="IP address to scan")
-    smbparser.add_argument("-results_file", type=str, required=False, help="Results File")
-    smbparser.add_argument("-packet_rate", default=1, type=int, required=False, help="Packet rate")
+    smbparser.add_argument("-netrange", type=str, required=False, help="CIDR Block")  # noqa
+    smbparser.add_argument("-ip", type=str, required=False, help="IP address to scan")  # noqa
+    smbparser.add_argument("-results_file", type=str, required=False, help="Results File")  # noqa
+    smbparser.add_argument("-packet_rate", default=1, type=int, required=False, help="Packet rate")  # noqa
     smbargs = smbparser.parse_args()
-    
+
     semaphore = threading.BoundedSemaphore(value=smbargs.packet_rate)
     print_lock = threading.Lock()
 
-    if smbargs.ip is not None: 
-        results = smbscan(smbargs.ip, smbargs.results_file)
-
+    if smbargs.ip is not None:
+#        smb_scan(smbargs.ip, smbargs.results_file)
+        smb_verify(smbargs.ip)
     elif smbargs.netrange is not None:
         for ip in netaddr.IPNetwork(smbargs.netrange).iter_hosts():
-            smbscan(str(ip), smbargs.results_file)
-
+#            smb_scan(str(ip), smbargs.results_file)
+            smb_verify(str(ip))
     elif smbargs.packet_rate is not None:
-       for ip in netaddr.IPNetwork(smbargs.netrange).iter_hosts():
-           semaphore.acquire()
-           smbthread = threading.Thread(target=thread_check, args=(str(ip), smbargs.results_file))
-           smbthread.start()
-           smbthread.join()
+        for ip in netaddr.IPNetwork(smbargs.netrange).iter_hosts():
+            semaphore.acquire()
+            smbthread = threading.Thread(target=thread_check, args=(str(ip), smbargs.results_file))  # noqa
+            smbthread.start()
+            smbthread.join()
 
-
-    else: 
-        print "Please define either IP or Netrange." 
+    else:
+        print "Please define either IP or Netrange."
         exit
