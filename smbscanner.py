a4c604680bdb73d8bc4a02dce29b218a074057ed
root <root@ostack5.CISCO>
2018-04-06 22:23:41 -0400

    feat/threading: removed semaphores and increased threading
    - scan time from 255 IPs reduced from 17.34 minutes to 1.2 minutes

--- smbscanner.py
+++ smbscanner.py
@@ -29,61 +29,55 @@ def smbscan(server, port, smbversion, results_file):
                 smbbg = connector.recv(2048)
                 connector.close()
                 if results_file is not None:
-                    with print_lock:
-                        with open(results_file, 'a+') as outfile:
-                            smb_data = ('host: ' + '%s' % server + '\n' +
-                                        'is_smb: true\nsmb_ver: ' +
-                                        smbversion + '\nopen_share:' +
-                                        '%s' % entry + '\n' + 'banner: ' +
-                                        '%s' % smbbg +
-                                        'is_dupulsar: true\nsmb_port: ' +
-                                        '%s' % port + '\nbg_port: 445\ntimestamp: ' +  # noqa
-                                        '%s' % ts + '\n\n')
-                            outfile.write(smb_data)
+                    with open(results_file, 'a+') as outfile:
+                        smb_data = ('host: ' + '%s' % server + '\n' +
+                                    'is_smb: true\nsmb_ver: ' +
+                                    smbversion + '\nopen_share:' +
+                                    '%s' % entry + '\n' + 'banner: ' +
+                                    '%s' % smbbg +
+                                    'is_dupulsar: true\nsmb_port: ' +
+                                    '%s' % port + '\nbg_port: 445\ntimestamp: ' +  # noqa
+                                    '%s' % ts + '\n\n')
+                        outfile.write(smb_data)
                 else:
-                    with print_lock:
-                        sys.stdout.write("[+] " + '%s' % server + ": " + '%s' % entry +  # noqa
-                                         ", Banner Grab: " + '%s' % smbbg +
-                                         ' Possible DPulsar Target: True' +
-                                         ', is_smb: True, smb_ver: ' + smbversion + '\n')  # noqa
+                    sys.stdout.write("[+] " + '%s' % server + ": " + '%s' % entry +  # noqa
+                                     ", Banner Grab: " + '%s' % smbbg +
+                                     ' Possible DPulsar Target: True' +
+                                     ', is_smb: True, smb_ver: ' + smbversion + '\n')  # noqa
             except:
                 if results_file is not None:
-                    with print_lock:
-                        with open(results_file, 'a+') as outfile:
-                            smb_data = ('host: ' + '%s' % server + '\n' +
-                                       'is_smb: true\nsmb_ver: ' + smbversion +
-                                       '\nopen_share: ' +  '%s' % entry + '\n' +  # noqa
-                                       'banner: closed\nis_dpulsar: false\nsmb_port: ' +  # noqa
-                                       '%s' % port + '\nbg_port: 445\ntimestamp: ' +  # noqa
-                                       '%s' % ts + '\n\n')
-                            outfile.write(smb_data)
+                    with open(results_file, 'a+') as outfile:
+                        smb_data = ('host: ' + '%s' % server + '\n' +
+                                    'is_smb: true\nsmb_ver: ' + smbversion +
+                                    '\nopen_share: ' +  '%s' % entry + '\n' +  # noqa
+                                    'banner: closed\nis_dpulsar: false\nsmb_port: ' +  # noqa
+                                    '%s' % port + '\nbg_port: 445\ntimestamp: ' +  # noqa
+                                    '%s' % ts + '\n\n')
+                        outfile.write(smb_data)
                 else:
-                    with print_lock:
-                        sys.stdout.write("[+] " + '%s' % server + ": " + '%s' % entry +  # noqa
-                                         ", Port 445: closed, Possible DPulsar Target = False, " +  # noqa
-                                         'is_smb: True, smb_ver: ' + smbversion + '\n')  # noqa
+                    sys.stdout.write("[+] " + '%s' % server + ": " + '%s' % entry +  # noqa
+                                     ", Port 445: closed, Possible DPulsar Target = False, " +  # noqa
+                                     'is_smb: True, smb_ver: ' + smbversion + '\n')  # noqa
             finally:
                 pass
         else:
             pass
     except:
         if results_file is not None:
-            with print_lock:
-                with open(results_file, 'a+') as outfile:
-                    smb_data = ('host: ' + '%s' % server + '\n' +
-                                'is_smb: true\nsmb_ver: ' + smbversion +
-                                '\nopen_share: no open shares were found. awesome!\n' +  # noqa
-                                'banner: closed\nis_dpulsar: false\nsmb_port: ' +  # noqa
-                                '%s' % port + '\nbg_port: 445\n' +  # noqa
-                                'timestamp: ' + '%s' % ts + '\n\n')
-                    outfile.write(smb_data)
-                    pass
-        else:
-            with print_lock:
-                sys.stdout.write("[+] " + '%s' % server + ": no open shares were found. awesome!, " +  # noqa
-                                 'Port 445: closed, Possible Dpulsar Target = False, ' +  # noqa
-                                 'is_smb: True, smb_ver: ' + smbversion + '\n')
+            with open(results_file, 'a+') as outfile:
+                smb_data = ('host: ' + '%s' % server + '\n' +
+                            'is_smb: true\nsmb_ver: ' + smbversion +
+                            '\nopen_share: no open shares were found. awesome!\n' +  # noqa
+                            'banner: closed\nis_dpulsar: false\nsmb_port: ' +  # noqa
+                            '%s' % port + '\nbg_port: 445\n' +  # noqa
+                            'timestamp: ' + '%s' % ts + '\n\n')
+                outfile.write(smb_data)
                 pass
+        else:
+            sys.stdout.write("[+] " + '%s' % server + ": no open shares were found. awesome!, " +  # noqa
+                             'Port 445: closed, Possible Dpulsar Target = False, ' +  # noqa
+                             'is_smb: True, smb_ver: ' + smbversion + '\n')
+            pass
     finally:
         pass
 
@@ -155,20 +149,6 @@ def smb_verify(server):
     finally:
         pass
 
-
-def thread_check(server, results_file):
-    global semaphore
-
-    try:
-        #        smb_scan(server, results_file)
-        smb_verify(server)
-    except Exception as e:
-        with print_lock:
-            logger.error('Scan error at ' + server + e, exc_info=True)  # noqa
-    finally:
-        semaphore.release()
-
-
 if __name__ == "__main__":
     smbparser = argparse.ArgumentParser(description="SMB Scanner")
     smbparser.add_argument("-netrange", type=str, required=False, help="CIDR Block")  # noqa
@@ -178,41 +158,27 @@ if __name__ == "__main__":
     smbparser.add_argument("-packet_rate", default=1, type=int, required=False, help="Packet rate")  # noqa
     smbargs = smbparser.parse_args()
 
-    semaphore = threading.BoundedSemaphore(value=smbargs.packet_rate)
-    print_lock = threading.Lock()
 
     if smbargs.ip is not None:
-        #        smb_scan(smbargs.ip, smbargs.results_file)
         smb_verify(smbargs.ip)
 
     elif smbargs.netrange is not None:
         for ip in netaddr.IPNetwork(smbargs.netrange).iter_hosts():
-            #            smb_scan(str(ip), smbargs.results_file)
-            smb_verify(str(ip))
+            threads = [smbargs.packet_rate]
+            smbthread = threading.Thread(target=smb_verify, args=(str(ip),))
+            threads.append(smbthread)
+            smbthread.start()
 
     elif smbargs.target_file is not None:
         with open(smbargs.target_file, mode='r', buffering=1) as targets_file:
             targets = targets_file.readlines()
             for target in targets:
                 for ip in netaddr.IPNetwork(target).iter_hosts():
-                    smb_verify(str(ip))
 
-    elif not smbargs.packet_rate and smbargs.target_file:
-        with open(smbargs.target_file, mode='r', buffering=1) as targets_file:
-            targets = targets_file.readlines()
-            for target in targets:
-                for ip in netaddr.IPNetwork(target).iter_hosts():
-                    semaphore.acquire()
-                    smbthread = threading.Thread(target=thread_check, args=(str(ip), smbargs.results_file))  # noqa
+                    threads = [smbargs.packet_rate]
+                    smbthread = threading.Thread(target=smb_verify, args=(str(ip),))
+                    threads.append(smbthread)
                     smbthread.start()
-                    smbthread.join()
-
-    elif not smbargs.packet_rate and smbargs.netrange:
-        for ip in netaddr.IPNetwork(smbargs.netrange).iter_hosts():
-            semaphore.acquire()
-            smbthread = threading.Thread(target=thread_check, args=(str(ip), smbargs.results_file))  # noqa
-            smbthread.start()
-            smbthread.join()
 
     else:
         print "Please define either IP or Netrange."
